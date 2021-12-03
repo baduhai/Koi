98db36160f70fcea8f68fc4af352a322ceeeca97
Vincent ROGER <roger.vins.11@gmail.com>
2021-12-03 22:49:13 +0100

    Fix issue #33.
    Instead of using a fixed path to plasma-changeicons now Koi locate the
    program path.

--- src/icons.cpp
+++ src/icons.cpp
@@ -8,7 +8,18 @@ Icons::Icons()
 void Icons::setIcons(QString iconTheme)
 {
     iconProcess = new QProcess;
-    QString program = "/usr/lib/plasma-changeicons";
+
+    // locate plasma-changeicons program
+    QString locateProgram = "locate";
+    QStringList programToLocate = {"plasma-changeicons"};
+
+    iconProcess->start(locateProgram, programToLocate);
+    iconProcess->waitForFinished();
+
+    QString program(iconProcess->readAllStandardOutput());
+    program.replace("\n", "");
+
+    // apply the icon theme
     QStringList arguments{iconTheme};
     iconProcess->start(program, arguments);
 }
