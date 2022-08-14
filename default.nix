bcf7d6bc2cd980492d378623dc285f5de5153059
William <williamhai@hotmail.com>
2022-08-14 21:30:18 +0200

    Added nix support

---
+++ default.nix
@@ -0,0 +1,31 @@
+{ lib, mkDerivation, fetchFromGitHub, cmake, qtbase, wrapQtAppsHook, kcoreaddons, kwidgetsaddons, kconfig }:
+
+mkDerivation rec{
+  name = "koi";
+  version = "0.1.5";
+  
+  src = fetchFromGitHub {
+    owner = "baduhai";
+    repo = "Koi";
+    rev = "${version}";
+    sha256 = "XVS/GWy8sB/B3tZ/ZSBqt0vVkzlGaW8zgqCBaFf+uzQ=";
+  };
+  
+  nativeBuildInputs = [ cmake ];
+  
+  buildInputs = [ wrapQtAppsHook kcoreaddons kwidgetsaddons kconfig ];
+  
+  sourceRoot = "source/src";
+  
+  installPhase = ''
+    mkdir -p $out/bin
+    mv Koi $out/bin
+  '';
+  
+  meta = with lib; {
+    description = "Theme scheduling for the KDE Plasma Desktop";
+    homepage = "https://github.com/baduhai/Koi";
+    license = licenses.lgpl3Plus;
+    platforms = platforms.linux;
+  };
+}
---
+++ dev.nix
@@ -0,0 +1,28 @@
+{ lib, mkDerivation, fetchFromGitHub, cmake, qtbase, wrapQtAppsHook, kcoreaddons, kwidgetsaddons, kconfig }:
+
+# Use this file either for nix-shell or for nix-build when developing locally.
+# nix-shell -E 'with import <nixpkgs> {}; pkgs.libsForQt5.callPackage ./dev.nix {}'
+# nix-build -E 'with import <nixpkgs> {}; pkgs.libsForQt5.callPackage ./dev.nix {}'
+# See default.nix to build release version.
+
+mkDerivation rec{
+  name = "koi";
+  
+  src = ./src;
+  
+  nativeBuildInputs = [ cmake ];
+  
+  buildInputs = [ wrapQtAppsHook kcoreaddons kwidgetsaddons kconfig ];
+  
+  installPhase = ''
+    mkdir -p $out/bin
+    mv Koi $out/bin
+  '';
+  
+  meta = with lib; {
+    description = "Theme scheduling for the KDE Plasma Desktop";
+    homepage = "https://github.com/baduhai/Koi";
+    license = licenses.lgpl3Plus;
+    platforms = platforms.linux;
+  };
+} 
