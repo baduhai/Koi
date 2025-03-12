ed4f022eaa7aed5c959ca8f9749d8da54c0f939e
Spencer Williams <spencerwi@gmail.com>
2025-03-12 10:46:16 -0400

    Add DBus interface for changing light/dark mode

---
+++ src/dbusinterface.cpp
@@ -0,0 +1,30 @@
+#include <QObject>
+#include "headers/utils.h"
+#include "koiadaptor.h"
+
+KoiDbusInterface::KoiDbusInterface(QObject* parent) : QObject(parent) {
+    new KoiDbusInterfaceAdaptor(this);
+    QDBusConnection dbus = QDBusConnection::sessionBus();
+    dbus.registerObject("/Koi", this);
+    dbus.registerService("dev.baduhai.Koi");
+    this->utils.initialiseSettings();
+}
+
+KoiDbusInterface::~KoiDbusInterface() {
+    QDBusConnection dbus = QDBusConnection::sessionBus();
+    dbus.unregisterService("dev.baduhai.Koi");
+    dbus.unregisterObject("/Koi");
+}
+
+
+void KoiDbusInterface::goLightMode() {
+    utils.goLight();
+}
+
+void KoiDbusInterface::goDarkMode() {
+    utils.goDark();
+}
+
+void KoiDbusInterface::toggleMode() {
+    utils.toggle();
+}
---
+++ src/headers/dbusinterface.h
@@ -0,0 +1,19 @@
+#include <QObject>
+#include "headers/utils.h"
+
+class KoiDbusInterface : public QObject {
+    Q_OBJECT
+    Q_CLASSINFO("Koi D-Bus Interface", "dev.baduhai.Koi")
+
+    public:
+        KoiDbusInterface(QObject* parent);
+        ~KoiDbusInterface();
+
+    public Q_SLOTS:
+        void goLightMode();
+        void goDarkMode();
+        void toggleMode();
+
+    private:
+        Utils utils;
+};
