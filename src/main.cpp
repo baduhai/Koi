4bf5c6434df7b938e6e0844ff28e198aefd49aff
Matthias Möller <m_moeller@live.de>
2025-04-25 15:42:51 +0200

    Replaces instance check with shared memory; Removes QT Network dependency (#113)
    Koi checks, if another instance is running.
    
    Current behaviour will start a server on a socket. If the socket is free, Koi starts, otherwise it refuses.
    
    This commit replaces the server mechanic with a 8byte shared memory block. Only 1 single Koi instance can have exclusive access to it. If the Koi instance cannot get exclusive access to the shared memory block, it will not start. If Koi quits normally or crashes, the OS will free up the shared memory access.
    
    Since we can remove the whole QNetwork dependency + do not have to listen to a socket, the memory usage is further reduces by ~2MB (mostly due to not loading libQt6Network.so).
    
    It is also unlikely, that QNetwork will be required by Koi in the near future.

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,29 @@
 #include "headers/mainwindow.h"
 #include "headers/utils.h"
 #include "headers/dbusinterface.h"
+#include "headers/runguard.h"
 
 #include <iostream>
 #include <memory>
 #include <QApplication>
-#include <QLocalSocket>
-#include <QLocalServer>
-
-bool isAlreadyRunning(QString netName)
-{
-    QLocalSocket socket;
-    socket.connectToServer(netName);
-    bool isOpen = socket.isOpen();
-    socket.close();
-    return isOpen;
-}
-
-std::unique_ptr<QLocalServer> createDummyNetwork(QString netName)
-{
-    auto server = std::make_unique<QLocalServer>();
-    server->setSocketOptions(QLocalServer::WorldAccessOption);
-    server->listen(netName);
-    return server;
-}
 
 int main(int argc, char *argv[])
 {
-    if (isAlreadyRunning("koiDummyNetwork"))
+    RunGuard rg("koiDummyNetwork");
+    if (!rg.tryToRun())
     {
         std::cout << "Another instance of Koi is already running" << std::endl;
+        return 1;
     }
-    else
+
+    Utils utils;
+    utils.initialiseSettings();
+    QApplication a(argc, argv);
+    MainWindow w;
+    if (utils.settings->value("start-hidden").toBool() == 0)
     {
-        const auto dummyServer = createDummyNetwork("koiDummyNetwork");
-        Utils utils;
-        utils.initialiseSettings();
-        QApplication a(argc, argv);
-        MainWindow w;
-        if (utils.settings->value("start-hidden").toBool() == 0)
-        {
-            w.show();
-        }
-        KoiDbusInterface dbusIf(&a);
-        return a.exec();
+        w.show();
     }
+    KoiDbusInterface dbusIf(&a);
+    return a.exec();
 }
